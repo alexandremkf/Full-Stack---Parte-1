7a2fecff1613dd6155eb93f7f4f10172246321d4
alexandremkf <alexandrematiellokf@gmail.com>
2025-04-03 16:09:56 -0300

    Fiz o 1.5 que era para transformar não só as partes em um array mas tudo dentro do App.

--- src/App.jsx
+++ src/App.jsx
@@ -25,31 +25,34 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1} 
-        part2={part2} exercises2={exercises2} 
-        part3={part3} exercises3={exercises3} 
-      />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course = {course} />
+      <Content parts = {parts} />
+      <Total parts = {parts} />
     </div>
   );
 };
