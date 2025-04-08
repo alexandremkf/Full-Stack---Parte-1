28e3c8331b8c18bd7e251fff985381566be75f5c
alexandremkf <alexandrematiellokf@gmail.com>
2025-04-08 01:24:17 -0300

    Fiz o 1.6, que é somente iniciando o projeto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,16 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({name, exercises}) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
-  return (
-    <div>
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
-    </div>
-  );
-};
-
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
-};
+import { useState } from 'react'
 
 const App = () => {
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  ]
-}
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course = {course} />
-      <Content parts = {parts} />
-      <Total parts = {parts} />
+      code here
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
