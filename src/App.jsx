d3d8cadcc679ed8a4b5f37ff3683d4cf7fececab
alexandremkf <alexandrematiellokf@gmail.com>
2025-04-08 02:37:34 -0300

    Fiz de vez os 1.12, 1.13 e o 1.14, que era para criar um projeto de anedotas onde mostrava elas e ia computando os seus votos de preferências e em baixo mostrava qual era a mais votada.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,46 @@
 import { useState } from 'react'
 
-const StatisticLine = ({ text, value }) => {
-  return <p>{text} {value}</p>
-}
-
-const Statistics = ({good, neutral, bad}) => {
-  const total = good + neutral + bad
-
-  if (total == 0) {
-    return <p>No feedback given</p>
-  }
-  
-  const average = (good - bad) / total
-  const positive = (good / total) * 100
-
-  return(
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
-    </div>
-  )
-}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const randomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const voteAnecdote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const maxVotes = Math.max(...votes)
+  const mostVotedIndex = votes.indexOf(maxVotes)
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteAnecdote}>vote</button>
+      <button onClick={randomAnecdote}>next anecdote</button>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-
-      <h1>statistics</h1>
-
-      <Statistics good={good} neutral={neutral} bad={bad} />
-
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>has {maxVotes} votes</p>
     </div>
   )
 }
